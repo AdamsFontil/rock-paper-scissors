70a0f9ecfed2cb26d56793151eeaa112bb1f77bd
Adams Fontil <adamsfuntil@gmail.com>
2022-09-10 21:26:06 -0400

    Remove all console.logs and other unneccessary lines of code to reduce number of lines in the program

--- script2.js
+++ script2.js
@@ -10,7 +10,7 @@ function getComputerChoice() {  // this function will let the computer pick at r
     return cPick  // this what this function ultimately returns, one choice rock, paper, or scissors.
 
 }
-let result;
+let roundResult;
 
 function playRound(userPick, cPick) { // I want to put the userpick (rock), and computerPick(random) against each other
     if ( cPick === userPick ) {
@@ -19,48 +19,41 @@ function playRound(userPick, cPick) { // I want to put the userpick (rock), and 
 
 
     } else if (cPick === "Paper" && userPick === "Rock") {
-        result = ("loser")
+        roundResult = ("loser")
         return("You lost! Paper beats Rock!")
     } else if (cPick === "Rock" && userPick === "Scissors") {
-        result = ("loser")
+        roundResult = ("loser")
         return("You lost! Rock beats Scissors")
     } else if (cPick === "Scissors" && userPick === "Paper") {
-        result = ("loser")
+        roundResult = ("loser")
         return("You lost! Scissors beats Paper!")
 // seperation made intentionally to differentiate between winning, losing and draw sections.
 
     } else if ( cPick === "Scissors" && userPick === "Rock") {
-        result = ("winner")
+        roundResult = ("winner")
         return ("You won! Rock beats scissors!")
 
     } else if ( cPick === "Paper" && userPick === "Scissors") {
-        result = ("winner")
+        roundResult = ("winner")
         return ("You won! Scissors beats Paper!")
 
     } else if ( cPick === "Rock" && userPick === "Paper") {
-        result = ("winner")
+        roundResult = ("winner")
         return ("You won! Paper beats Rock!")
     }
-    console.log(result)
 }
 
 function game () {
-    if (result === "winner") { // if playRound returns winner the user's points increases
+    if (roundResult === "winner") { // if playRound returns winner the user's points increases
     uPoints ++
-    console.log(`your points : ${uPoints}`)
-} else if (result === "loser") { // if the playRound function returns loser than the computer's points increases
+} else if (roundResult === "loser") { // if the playRound function returns loser than the computer's points increases
     cPoints ++
-    console.log(`pc points : ${cPoints}`)
 }
 if ( cPoints === 5 && cPoints > uPoints) { //if after the loop stops cPoints if higher than the computer has won the game.
-        console.log ("Sorry, the computer beat you")
-        const result = document.querySelector('.result')
         result. textContent = ('Sorry, the computer beat you.')
         cPoints = 0;
         uPoints = 0;
     } else if (uPoints === 5 && uPoints > cPoints) { // if after the loop stops and uPoints is higher than the user has won.
-        console.log ("You did it! You beat the computer at RPS")
-        const result = document.querySelector('.result')
         result. textContent = ('You did it! You beat the computer at RPS.')
         cPoints = 0;
         uPoints = 0;
@@ -68,60 +61,37 @@ if ( cPoints === 5 && cPoints > uPoints) { //if after the loop stops cPoints if 
     const uScore = document.querySelector('.uScore')
     uScore.textContent = (`Your score: ${uPoints}`)
     const cScore = document.querySelector('.cScore')
-    cScore.textContent = (`Your score: ${cPoints}`)
-
+    cScore.textContent = (`Computer's score: ${cPoints}`)
 }
 
 
 let userPick;
-let counter = 0;
-let counter2 = 0;
-let counter3 = 0;
 let uPoints = 0;
 let cPoints = 0;
-
+const result = document.querySelector('.result')
 
 const rock = document.querySelector(".rock");
 rock.addEventListener('click', () => {
-  counter ++
-//   console.log(`rock: ${counter}`)
   userPick = "Rock"
   getComputerChoice()
-  console.log(userPick)
-  console.log(getComputerChoice())
   playRound(userPick,cPick);
-//   console.log(playRound(userPick,cPick));
-    const result = document.querySelector('.result')
-    result.textContent = (playRound(userPick, cPick))
-    game()
+  result.textContent = (playRound(userPick, cPick))
+  game()
 });
 const paper = document.querySelector(".paper");
 paper.addEventListener('click', () => {
-  counter2 ++
   userPick = "Paper"
   getComputerChoice()
-//   console.log(`paper: ${counter2}`)
-  console.log(userPick)
   console.log(getComputerChoice())
   playRound(userPick,cPick);
-//   console.log(playRound(userPick,cPick));
-    const result = document.querySelector('.result')
-    result.textContent = (playRound(userPick, cPick))
-    game()
+  result.textContent = (playRound(userPick, cPick))
+  game()
 });
-
 const scis = document.querySelector(".scis");
 scis.addEventListener('click', () => {
-  counter3 ++
   userPick = "Scissors"
   getComputerChoice()
-//   console.log(getComputerChoice())
-//   console.log(`Scissors: ${counter3}`)
-//   console.log(userPick)
   playRound(userPick,cPick);
-//   console.log(playRound(userPick,cPick));
-    const result = document.querySelector('.result')
-    result.textContent = (playRound(userPick, cPick))
-    game()
-
+  result.textContent = (playRound(userPick, cPick))
+  game()
 });
